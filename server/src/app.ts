92b21c0eb3b307c9db8cbca3df78e7855e95bf49
Rashedin-063 <rashedinislam.06@gmail.com>
2025-09-08 15:49:56 +0600

    server setup done

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,36 @@
+import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { notFoundHandler, globalErrorHandler } from 'express-error-toolkit';
 import { StatusCodes } from 'http-status-toolkit';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
-// cors and body parser
-app.use(cors());
-app.use(express.json());
+// security middleware
+const corsOption = {
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  credentials: true
+}
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.',
+  standardHeaders: true, // return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // disable the `X-RateLimit-*` headers
+});
+
+app.use(helmet());
+app.use(cors(corsOption));
+app.use(limiter)
+
+
+// body parser
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+
 
 // home route
 app.get('/', (_req: Request, res: Response) => {
