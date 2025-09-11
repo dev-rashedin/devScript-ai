ea120be7497eded62b411499e9c05f11721ce1b4
Rashedin-063 <rashedinislam.06@gmail.com>
2025-09-11 16:39:00 +0600

    error page updated

--- client/src/component/Error.tsx
+++ client/src/component/Error.tsx
@@ -1,5 +1,17 @@
-const Error = ({ error } : { error: string | undefined }) => {
-  return <p className='text-red-500 mt-3'>{error}</p>;
+import { Link } from 'react-router';
+import Button from '../components/Button';
+import Lottie from 'lottie-react';
+import Animation from '../assets/Animation.json';
+
+const ErrorPage = () => {
+  return (
+    <div className='min-h-screen flex flex-col justify-center items-center'>
+      <Lottie animationData={Animation} style={{ height: '300px' }} />
+      <Link to='/'>
+        <Button label='Go to Home' type='primary'></Button>
+      </Link>
+    </div>
+  );
 };
 
-export default Error;
+export default ErrorPage;
