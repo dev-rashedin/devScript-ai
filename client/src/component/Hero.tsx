cda5868bd29024c3a8c46cd9bcb829e5ff14f4cb
Rashedin-063 <rashedinislam.06@gmail.com>
2025-09-17 20:11:29 +0600

    hero section updated

--- client/src/component/Hero.tsx
+++ client/src/component/Hero.tsx
@@ -2,57 +2,59 @@
 
 import { sponsorLogos } from '../data';
 import Button from './ui/Button';
-import FloatingIcons from './ui/FloatingIcons';
 import HighlightedText from './ui/HighlightedText';
 import Marquee from 'react-fast-marquee';
+import banner from '../assets/banner.png';
 
 
 const repeatedLogos = Array.from({ length: 3 }, () => sponsorLogos).flat();
 
 const Hero = () => {
   return (
-    <main className='h-[91.3vh] flex flex-col items-center justify-center space-y-28 '>
-      <section className='mt-20 text-center max-w-3xl  mx-auto px-1 relative'>
+    <main className='h-[91.3vh] overflow-y-hidden boundary flex flex-col items-center justify-center'>
+      <section className='flex flex-col-reverse items-center justify-center xl:px-12 xl:flex-row xl:items-center xl:justify-center overflow-hidden'>
         {/* hero text and buttons */}
-        <div>
-          {/* <p className='text-blue-700'>Your Ultimate Productivity tool</p> */}
-          <h1 className=''>AI Assistant for</h1>
+        <div className='max-w-3xl flex flex-col justify-center items-center text-center  xl:items-start xl:text-left '>
+          <h1>AI Assistant for</h1>
           <HighlightedText label='Developers & Writers' />
 
-          <p className='md:text-lg lg:text-xl text-muted mt-4 lg:mt-8'>
+          <p className='md:text-lg lg:text-xl text-muted mt-6 xl:mt-9'>
             Explain code, refactor smarter, summarize documents, write articles,
             craft emails, and polish your resume — all in one place.
           </p>
 
-          <div className='flex-center gap-4 mt-2 lg:mt-8'>
+          <div className='flex-center gap-4 mt-8 xl:mt-12'>
             <Button label='Get Started' type='primary' />
             <Button label='View Services' type='secondary' href='#services' />
           </div>
 
-          <p className='text-muted text-[14px] mt-4 text-center'>
+          <p className='max-w-2xl text-muted text-xs mt-4 text-center xl:mt-8 xl:text-left'>
             <span className='font-bold'>Note:</span> Some features described
             here might be planned in our roadmap but not yet released. You can
             open a live chat from within your account and our Support team can
             offer more details or help you with workarounds.
           </p>
         </div>
-
-        {/*  floating icons */}     
-     <FloatingIcons/>
+        {/* hero image */}
+        <div className='hidden md:block w-[400px] h-auto lg:w-[400px] xl:w-[900px]'>
+          <img src={banner} alt='coding' className='w-[1000px] h-full' />
+        </div>
+        {/*  floating icons */}
+        {/* <FloatingIcons/> */}
       </section>
 
       {/* bottom marquee */}
-      <section className='w-[100%]  bg-faded-pearl'>
-          <Marquee direction='left' speed={80} gradient={false}>
-            {repeatedLogos.map((logo) => (
-              <img
-                key={`${logo.id}-${Math.random()}`}
-                src={logo.href}
-                alt={logo.name}
-                className='h-9 mr-20'
-              />
-            ))}
-          </Marquee>
+      <section className='w-[100%] mt-12 lg:mt-16  bg-faded-pearl'>
+        <Marquee direction='left' speed={80} gradient={false}>
+          {repeatedLogos.map((logo) => (
+            <img
+              key={`${logo.id}-${Math.random()}`}
+              src={logo.href}
+              alt={logo.name}
+              className='h-9 mr-20'
+            />
+          ))}
+        </Marquee>
       </section>
     </main>
   );
--- client/src/component/ui/FloatingIcons.tsx
+++ client/src/component/ui/FloatingIcons.tsx
@@ -5,6 +5,10 @@ import {
   AiOutlineMail,
   AiOutlineEdit,
 } from 'react-icons/ai';
+import emailIcon from '../../assets/email.png'
+import writingIcon from '../../assets/writing.png'
+import resumeIcon from '../../assets/resume.png'
+import codingIcon from '../../assets/ai-coding.png'
 
 const floatAnim = {
   animate: { y: [0, -15, 0] },
@@ -15,30 +19,32 @@ const FloatingIcons = () => {
   return (
     <div className='hidden lg:block'>
       {/* floating icons left */}
-      <div className='hidden md:block absolute left-[-120px] top-1/3 space-y-12'>
+      <div className='hidden md:block absolute left-[-180px] bottom-28 space-y-12'>
         <motion.div {...floatAnim}>
-          <AiOutlineCode size={40} className='text-primary' />
+          <img src={resumeIcon} alt='Email' className='w-96 h-96' />
         </motion.div>
+      </div>
+      <div className='hidden md:block absolute left-[-100px] -top-20 space-y-12'>
         <motion.div
           {...floatAnim}
           transition={{ ...floatAnim.transition, delay: 1 }}
         >
-          <AiOutlineFileText size={40} className='text-secondary' />
+          <img src={writingIcon} alt='Email' className='w-36 h-36' />
         </motion.div>
       </div>
 
       {/* floating icons right */}
-      <div className='hidden md:block absolute right-[-80px] xl:right-[-120px] top-1/4 space-y-12'>
+      <div className='hidden md:block absolute right-[-120px] xl:right-[-300px] top-1/4 space-y-12'>
         <motion.div {...floatAnim}>
-          <AiOutlineMail size={40} className='text-accent' />
+          <img src={emailIcon} alt='Email' className='w-36 h-36' />
         </motion.div>
       </div>
-      <div className='hidden md:block absolute right-[-120px] xl:right-[-180px] top-1/2 space-y-12'>
+      <div className='hidden md:block absolute right-[-120px] xl:right-[-180px] -top-20 space-y-12'>
         <motion.div
           {...floatAnim}
           transition={{ ...floatAnim.transition, delay: 1 }}
         >
-          <AiOutlineEdit size={40} className='text-primary' />
+          <img src={codingIcon} alt='Email' className='w-96 h-96' />
         </motion.div>
       </div>
     </div>
